cc1782c45179ae7cf9209f3a8a999ba80b84fa42
Muminjon <juramirzaev.muminjon@gmail.com>
2024-01-13 22:05:07 +0100

    project setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// Import components
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+// Import pages
+import Home from "./pages/Home";
+import PropertyDetails from "./pages/PropertyDetails";
 
 const App = () => {
-  return <div>react app</div>;
+  return (
+    <div className="max-w-[1440px] mx-auto bg-white">
+      {" "}
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/property/:id" element={<PropertyDetails />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </div>
+  );
 };
 
 export default App;
