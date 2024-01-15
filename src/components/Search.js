8aa21c8798a8638d0939734560cbdfb46a384845
Muminjon <juramirzaev.muminjon@gmail.com>
2024-01-15 22:16:15 +0100

    house list built

--- src/components/Search.js
+++ src/components/Search.js
@@ -12,6 +12,7 @@ import { RiSearch2Line } from "react-icons/ri";
 import { HouseContext } from "./HouseContext";
 
 const Search = () => {
+  const { handleClick } = useContext(HouseContext);
   return (
     <div
       className="px-[30px] py-6 max-w-[1170px] mx-auto flex flex-col lg:flex-row
@@ -22,6 +23,7 @@ const Search = () => {
       <PropertyDropDown />
       <PriceRangeDropDown />
       <button
+        onClick={() => handleClick()}
         className="bg-violet-600 hover:bg-violet-800 transition w-full lg:max-w-[162px]
       h-12 rounded-lg flex justify-center items-center text-white text-lg"
       >
